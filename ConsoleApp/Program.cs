81ce705dd8f84fd8743b5ba60379c5043e767b82
Tysuiku <timothydong41@gmail.com>
2023-08-18 00:18:28 -0400

    notes

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//
+//Variables
+//
+
+//Textbook Def = Names given to data we need to store in memory
+
+int a = 99; // not a decimal number
+string b = "abc"; 
+byte c = 255; //0-255
+char d = 'a';
+double testDouble = 14.43; // 5.0X10^308 15 pre
+float testFloat = 0.11156154f; //5.0X10^38 7 pre
+decimal testDecimal = .121212m; //28 digits of precision 
+
